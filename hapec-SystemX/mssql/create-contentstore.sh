21b7d6f726a95614c7760c662700ce7ff7b2fd86
Michael Sommer <michael.sommer@hapec.de>
2023-07-25 14:30:19 +0000

    mssql container upgraded to Microsoft SQL-Server 2022:latest

--- hapec-SystemX/mssql/create-contentstore.sh
+++ hapec-SystemX/mssql/create-contentstore.sh
@@ -2,4 +2,4 @@ echo "waiting for the SQL-Server to come up"
 sleep 40s
 
 echo "checking for content store database"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -i create-contentstore.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${MSSQL_SA_PASSWORD} -i create-contentstore.sql
