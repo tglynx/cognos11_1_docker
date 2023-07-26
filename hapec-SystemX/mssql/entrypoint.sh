ee28a0dc4dafb133e64fec143b21a1cd361ede9b
Michael Sommer <michael.sommer@hapec.de>
2023-07-26 16:33:21 +0000

    log lines fix (mssql container)

--- hapec-SystemX/mssql/entrypoint.sh
+++ hapec-SystemX/mssql/entrypoint.sh
@@ -1,3 +1,3 @@
 #start SQL Server, start the script to create the DB and import the data, start the app
 echo "Starting Microsoft SQL-Server Express on Linux (2019) - System X Content Store Database"
-/usr/mssql/create-contentstore.sh & /opt/mssql/bin/sqlservr
+/usr/mssql/systemx-contentstore.sh & /opt/mssql/bin/sqlservr | tr -d '\r'
--- hapec-SystemX/mssql/systemx-contentstore.sh
+++ hapec-SystemX/mssql/systemx-contentstore.sh
@@ -1,5 +1,7 @@
 echo "waiting for the SQL-Server to come up"
-sleep 40s
+sleep 20s
+#tail -n+0 /var/opt/mssql/log/errorlog | awk '{print "\033[34mContent Store:\033[0m",$0}'
+sleep 20s
 
 echo "checking for content store database"
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${MSSQL_SA_PASSWORD} -i create-contentstore.sql
